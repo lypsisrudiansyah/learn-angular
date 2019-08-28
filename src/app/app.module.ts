cb92dfbe35c2a6fec2b35e01ca246e51ed223460
rudiansyah <rudiansyahcrypto@gmail.com>
2019-08-28 19:06:22 +0700

    Learn manually and generated by generator component
    Learn styling external and inteernal and using the selector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 import { AppComponent } from './app.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
-import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertsComponent } from './warning-alerts/warning-alerts.component';
+import { SuccessAlertsComponent } from './success-alerts/success-alerts.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    SuccessAlertComponent,
-    WarningAlertComponent
+    ServerComponent,
+    ServersComponent,
+    WarningAlertsComponent,
+    SuccessAlertsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
